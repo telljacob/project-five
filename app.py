92eeca4b09c5a415cff09d24c5dce45284ef3fe1
Jacob <telljacob@gmail.com>
2022-06-21 04:15:45 +0000

    Fill in app.py file

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
 from app_pages.multipage import multipage
+
+from app_pages.page
+
+app = MultiPage(app_name= "Facial Recognition")
+
+app.add_page("Project Summary")
+app.add_page("Problemsolving Techniques")
+app.add_page("Data Collection")
+
+app.run() #Run the app
